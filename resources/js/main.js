37d8a57069c1d5794700fe1739ab5488f10613a9
itok toni <itok.toni@gmail.com>
2026-06-10 14:53:56 +0700

    remove unused file

--- resources/js/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './assets/main.css'
-import 'tom-select/dist/css/tom-select.default.min.css'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
--- resources/js/pages/Home.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="p-6">
-    <h1 class="headline-lg">WMS Portal</h1>
-    <p class="body-lg mt-4">Welcome to the Warehouse Management System Portal.</p>
-  </div>
-</template>
-
-<script setup>
-</script>
